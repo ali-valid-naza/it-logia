841079b8f424f5a3c2e1bfc2ead45c332e820622
OlegAnisimov <gramafon@yandex.ru>
2022-12-23 16:55:15 +0300

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateOrderComponent } from './components/create-order/create-order.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ModalElementComponent } from './components/modal-element/modal-element.component';
+import { PopupComponent } from './components/popup/popup.component';
+import { OrderFormComponent } from './components/order-form/order-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateOrderComponent,
+    AboutComponent,
+    FooterComponent,
+    HeaderComponent,
+    ModalElementComponent,
+    PopupComponent,
+    OrderFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/modal-element/modal-element.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-element',
+  templateUrl: './modal-element.component.html',
+  styleUrls: ['./modal-element.component.scss']
+})
+export class ModalElementComponent {
+
+}
